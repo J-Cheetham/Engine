e57d67a4293988d3ed6de11d30c8e4d17d944f9d
J_Cheetham <j20.jc@hotmail.com>
2021-05-03 10:42:37 +0100

    Setup basic App and entry point.

---
+++ Engine/src/Engine.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//For use by Engine applications
+//Set this up by going to Additional Include Directories in the project properties you're interested in and pasting $(SolutionDir)Engine\src;
+
+#include "Engine/Application.h"
+
+// ---Entry Point----------------------------
+#include "Engine/EntryPoint.h"
+// ------------------------------------------
---
+++ Engine/src/Engine/Core.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef ENG_PLATFORM_WINDOWS
+	#ifdef ENG_BUILD_DLL
+		#define ENG_API __declspec(dllexport)
+	#else
+		#define ENG_API __declspec(dllimport)
+	#endif
+
+#else
+	#error Eng only supports Windows!
+#endif
